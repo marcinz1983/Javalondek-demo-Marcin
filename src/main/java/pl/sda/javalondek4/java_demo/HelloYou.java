ebe0fa5b30b867a450abf647405f7943ce673d6a
Marcin <marcinzielinski1983@gmail.com>
2021-01-16 10:29:26 +0000

    [init] - initial project commit

---
+++ src/main/java/pl/sda/javalondek4/java_demo/HelloYou.java
@@ -0,0 +1,7 @@
+package pl.sda.javalondek4.java_demo;
+
+public class HelloYou {
+    public static void main(String[] args) {
+        System.out.println("Witajcie na zajeciach u manka");
+    }
+}
