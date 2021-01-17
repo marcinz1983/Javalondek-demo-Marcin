f9573e392412248774d350b25bf9f8801d250a0a
Marcin <marcinzielinski1983@gmail.com>
2021-01-17 13:11:17 +0000

    [change] - interface adder implementation in progres

---
+++ src/main/java/pl/sda/javalondek4/java_demo/lambda/calculator/Adder.java
@@ -0,0 +1,8 @@
+package pl.sda.javalondek4.java_demo.lambda.calculator;
+
+@FunctionalInterface
+public interface Adder {
+
+    int add(int first, int secound);
+
+}
---
+++ src/main/java/pl/sda/javalondek4/java_demo/lambda/calculator/CalcDemo.java
@@ -0,0 +1,21 @@
+package pl.sda.javalondek4.java_demo.lambda.calculator;
+
+public class CalcDemo {
+
+    public static void main(String[] args) {
+        System.out.println("playing with calculator lambdas");
+
+        Adder adder = (a,b) -> a+b;
+
+
+
+        StrangeOne strangeOne = (a,b) -> {return 1;};
+
+
+
+
+
+
+
+    }
+}
