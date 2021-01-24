85bdc0f11ed96a47da6a5ed6bde90aabb44fe202
Marcin <marcinzielinski1983@gmail.com>
2021-01-24 12:09:28 +0000

    [change] - playing with Optional<> class

--- src/main/java/pl/sda/javalondek4/java_demo/optional/MySecret.java
+++ src/main/java/pl/sda/javalondek4/java_demo/optional/MySecret.java
@@ -1,22 +1,27 @@
 package pl.sda.javalondek4.java_demo.optional;
 
+import pl.sda.javalondek4.java_demo.lambda.calculator.Operation;
+
+import java.util.Optional;
+
 import static java.util.Objects.nonNull;
 
-public class MySecret {
+public class MySecret<T>{
 
 
-    private String secret;
+    private T secret;
 
-    public MySecret(String secret) {
+    public MySecret(T secret) {
         this.secret = secret;
     }
 
-    public String getSecret() {
+    public T getSecret() {
         return secret;
     }
 
-    public void setSecret(String secret) {
+    public void setSecret(T secret) {
         this.secret = secret;
+
     }
 
     @Override
@@ -28,15 +33,26 @@ public class MySecret {
 
     public static void main(String[] args) {
 
-        MySecret realone = new MySecret("to jest sekret nie mow nikomu ;)");
-         MySecret withNullContent = new MySecret(null);
+        MySecret<String> realone = new MySecret<>("to jest sekret nie mow nikomu ;)");
+         MySecret<String> withNullContent = new MySecret<>(null);
          printSecretLength(realone);
          printSecretLength(withNullContent);
 
+
+         MySecret<Integer> secretNumber = new MySecret<>(42);
+         MySecret<Integer> secreNulltNumber = new MySecret<>(null);
+
+         var secretNumOne = secretNumber.getSecret();
+         var secretNull = secreNulltNumber;
+
+         //optional<Integer>
+        Optional<Integer> number = Optional.of(42);
+        Optional<Integer> nullNumber = Optional.empty();
+        Optional<Integer> nullNumber1 = Optional.of(secretNumber.getSecret());
     }
 
 
-    public static  void printSecretLength(MySecret mySecret){
+    public static  void printSecretLength(MySecret<String> mySecret){
 
         int secretLen =  mySecret.getSecret()!=null ? mySecret.getSecret().length() : 0;
 
@@ -51,6 +67,24 @@ public class MySecret {
 //        }
 
         System.out.println("My secret has : " + secretLen + "characters");
+    }
+
+
+    public  static<T>  void processSecret(T optional){
+        Optional<T> opt = Optional.ofNullable(optional);
+        int number = 4;
+        Optional<Integer> numberInsideOptional = Optional.of(number);
+
+       // T unpacked = opt.get(); don t do it!!
+
+        T unpacked;
+
+        unpacked = opt.orElse(null);
+        unpacked = (T) opt.or(() -> null);
+
+        Optional<String> string = Optional.empty();
+        Optional<String> fromOptional = string.or(() -> Optional.of("no value"));
+
     }
 
 
